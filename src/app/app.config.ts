b69da20148ceccce856b0d3099b5c2bd73da7b40
margotB <13nerubepe>
2026-06-13 20:53:14 +0100

    feat: ajout PWA service worker et manifest

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import {ApplicationConfig, isDevMode} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideIonicAngular } from '@ionic/angular/standalone';
 import { provideHttpClient } from '@angular/common/http';
+import {provideServiceWorker} from "@angular/service-worker";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     // provideIonicAngular(),
     provideHttpClient(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
+
+
   ]
 };
