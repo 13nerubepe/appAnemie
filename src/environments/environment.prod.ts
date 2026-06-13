a321290c3b08ffd9a096f1c14c62cc9ded8c4155
margotB <13nerubepe>
2026-06-13 01:20:05 +0100

    *

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://imas-ai-api.onrender.com'
+  apiUrl: 'https://modele-regression-ordinal-multiniveau-2.onrender.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,8 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:8000'
+
   // API_GATEWAY_URL: "http://localhost:7171",
   // API_AUTHSERVER_URL: "http://localhost:7000",
 
@@ -18,12 +15,3 @@ export const environment = {
   // WEB_SUIVI_URL: "http://localhost:4205",
   // WEB_EXECUTION_URL: "http://localhost:4205",
 }
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
