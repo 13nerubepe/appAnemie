606b0427055440e23e80552d8066a9343265f70a
mngono <bepeneru13@gmail.com>
2026-07-14 18:55:21 +0100

    feat: add web manifest for PWA support

--- src/main.ts
+++ src/main.ts
@@ -39,6 +39,12 @@ bootstrapApplication(AppComponent, {
           }), provideServiceWorker('ngsw-worker.js', {
             enabled: !isDevMode(),
             registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
           })
   ],
 });
